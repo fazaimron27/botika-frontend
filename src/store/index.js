7e439f3923fd6e879343184ce8e6da25d55aac21
fazaimron27 <cincaumyself@gmail.com>
2024-08-08 01:40:14 +0700

    feat: add store to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
+
+const initialState = JSON.parse(localStorage.getItem("store")) || {
+  isAuthenticated: false,
+};
 
 export const store = reactive({
-  isAuthenticated: false,
+  ...initialState,
   setAuthenticated(status) {
     this.isAuthenticated = status;
   },
 });
+
+watch(
+  () => store.isAuthenticated,
+  (newValue) => {
+    localStorage.setItem("store", JSON.stringify(store));
+  }
+);
